68268d77607b9aba7bca9533e221df43e952c90c
santiago <elsantihg@gmail.com>
2024-05-06 23:16:48 -0500

    cambios BACK Y DE FRONT

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
         rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></link>
       {/* Cuerpo de la página */}
       <body>{children}</body>
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
       <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></script>
     </html>
   );
---
+++ src/app/notas/page.jsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+import Button from '../components/modal/boton';
+import Nav from '../components/nav/nav';
+
+export default function Notas() {
+
+  const [notas, setNotas] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/notas');
+      const data = await response.json();
+      setNotas(data);
+    } catch (error) {
+      console.error('Error al obtener las notas:', error);
+    }
+  };
+
+  // Llama a fetchData cuando el componente se monte
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Nav /> 
+      <div>
+        <Button>hola</Button>
+      </div>
+      <div className='m-3'>
+        <h1>Nota</h1>
+        <div>
+          <div className="row text-center">
+            {notas.map(nota => (
+              <div key={nota._id} className="m-1 col-sm-2">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">{nota.nombre}</h5>
+                    <p className="card-text">{nota.nota}</p>
+                    <button className="btn btn-danger">Eliminar</button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
