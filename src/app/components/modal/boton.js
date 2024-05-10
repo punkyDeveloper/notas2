f6423345158bf1dbfde9790391cd9480930e2e87
santiago <elsantihg@gmail.com>
2024-05-09 22:45:21 -0500

    funciona apartado de notas

--- src/app/components/modal/boton.js
+++ src/app/components/modal/boton.js
@@ -4,27 +4,25 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 function Example() {
+  // boostrap
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-
+  //
   const [nombre, setNombre] = useState('');
   const [nota, setNota] = useState('');
   const [error, setError] = useState(null);
 
-
   const enviarNota = async (e) =>{
     e.preventDefault();
-    console.log("nombre",nombre)
-    console.log("nombre",nota)
     // validaciones
     if (!nombre || !nota) {
-      setError("Comple los campos de la nota.");
+      setError("Complete los campos de la nota.");
       return;
     }
-const userId = 123
+    const userId = 123;
     try {
       await fetch("/api/notas",{
         method: "POST",
@@ -32,19 +30,16 @@ const userId = 123
           "Content-type": "application/json",
         },
         body: JSON.stringify({ nombre, nota, userId }), // Pasar el ID del usuario al servidor
-      })
+      });
+      // Limpiar los campos después de enviar la nota
+      setNombre('');
+      setNota('');
     } catch (error) {
-      setError("Error al enviar la nota")
+      setError("Error al enviar la nota");
       console.error('Error al enviar la nota:', error);
-      return
     }
-
-    // Limpiar los campos después de enviar la nota
-    setNombre('');
-    setNota('');
   };
 
-  
   return (
     <>
       <Button className="m-3" variant="primary" onClick={handleShow}>
@@ -58,46 +53,49 @@ const userId = 123
         keyboard={false}
       >
         <form onSubmit={enviarNota}>
-        <Modal.Header closeButton>
-          <Modal.Title>Crea tu nota</Modal.Title>
+
+
+              <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <label className="text-zinc-800">Titulo:</label>
-          <input
-            onChange={(e) => setNombre(e.target.value)}
-            value={nombre}
-            type="text"
-            className="border-4 border-b-black px-4 py-2 block mb-2 w-full"
-            name="fullName"
-            id="fullName"
-          />
+                <label className="text-zinc-800">Titulo:</label>
+                <input
+                  onChange={(e) => setNombre(e.target.value)}
+                  value={nombre}
+                  type="text"
+                  className="border-4 border-b-black px-4 py-2 block mb-2 w-full"
+                  name="fullName"
+                  id="fullName"
+                />
   
-          <label className="text-zinc-800">Nota:</label>
-          <input
-            onChange={(e) => setNota(e.target.value)}
-            value={nota}
-            type="text"
-            className="border-4 border-b-black px-4 py-2 block mb-2 w-full"
-            name="text"
-            id="text"
-          />
+                <label className="text-zinc-800">Nota:</label>
+                <input
+                  onChange={(e) => setNota(e.target.value)}
+                  value={nota}
+                  type="text"
+                  className="border-4 border-b-black px-4 py-2 block mb-2 w-full"
+                  name="text"
+                  id="text"
+                />
+  
+                {/* Conditionally render error message */}
+                {error && (
+                  <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+                    <p className="font-bold">¡Atención!</p>
+                    <p>{error}</p>
+                  </div>
+                )}
+              </Modal.Body>
 
-        {/* Conditionally render error message */}
-        {error && (
-          <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
-            <p className="font-bold">¡Atención!</p>
-            <p>{error}</p>
-          </div>
-        )}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cerrar
-          </Button>
-          <Button type="submit" variant="primary">Guardar</Button>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>Cerrar</Button>
+                <Button variant="primary" type="submit">Crear nota</Button>
         </Modal.Footer>
+
+
         </form>
-      </Modal>
+        </Modal>
     </>
   );
 }
--- src/app/notas/page.jsx
+++ src/app/notas/page.jsx
@@ -2,8 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Buttons from '../components/modal/boton';
 import Nav from '../components/nav/nav';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+
 
 
 export default function Notas() {
@@ -17,7 +16,7 @@ export default function Notas() {
       if (Array.isArray(data) && data.length > 0) {
         setNotas(data);
       } else {
-        setError('No se encontraron notas.');
+        setError('No se encontraron notas, ingrese una nota');
       }
     } catch (error) {
       console.error('Error al obtener las notas:', error);
@@ -39,30 +38,32 @@ export default function Notas() {
       <div className='m-3'>
         <h1 >Nota</h1>
         <div>
-                  {/* Conditionally render error message */}
-        {error && (
-          <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
-            <p className="font-bold">¡Atención!</p>
+          {/* Conditionally render error message */}
+          {error && (
+          <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
             <p>{error}</p>
           </div>
-        )}
+          )}
 
 
+          <div className="row text-center m-5">
             {notas.map(nota => (
-
-              //  <Card  style={{ width: '18rem' }}>
-              <Card style={{ width: '18rem' }}>
-                      <Card.Header >{nota.nombre}</Card.Header>
-                      <Card.Body>
-                        <Card.Text>
-                        {nota.nota}
-                        </Card.Text>
-                        <Button variant="danger">Eliminar</Button>
-                      </Card.Body>
-                      </Card>
-
-
+              <div key={nota._id} className="m-1 col-sm-2">
+                <div className="card">
+                  <div className="card-body">  
+                    <div class="card-header">
+                      {nota.nombre}
+                    </div>
+                    
+                    <p className="card-text">{nota.nota}</p>
+                    {/* Aquí puedes agregar más detalles de la nota */}
+                    <button className="btn btn-primary m-2">Editar</button>
+                    <button className="btn btn-danger m-2">Eliminar</button>
+                  </div>
+                </div>
+              </div>
             ))}
+          </div> 
 
         </div>
       </div>
