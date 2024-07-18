443be975d67c4a4a0a20b196b2d30f02c0027cae
santiago <elsantihg@gmail.com>
2024-07-17 21:37:17 -0500

    response registrar

--- src/app/registrar/page.jsx
+++ src/app/registrar/page.jsx
@@ -7,28 +7,26 @@ function Signup() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
 
-
-
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // validaciones
+    // Validaciones
     if (!fullName || !email || !password) {
       setError("Por favor, complete todos los campos.");
       return;
     }
     if (password.length < 6) {
-      setError("la contraseña tiene que ser mayor a 6 dijitos.");
+      setError("La contraseña tiene que ser mayor a 6 dígitos.");
       return;
     }
 
-    // Envia los datos
+    // Enviar los datos
     try {
       const url = "/api/registrar"
       const response = await fetch(url, { 
         method: "POST",
         headers: {
-          "Content-type": "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           fullName,
@@ -37,8 +35,7 @@ function Signup() {
         }),
       });
 
-      // valida que si devuelva datos 
-
+      // Validar que devuelva datos 
       if (response.ok) {
         const data = await response.json();
         console.log("Respuesta del servidor:", data);
@@ -50,16 +47,12 @@ function Signup() {
       console.error(error);
       setError('Error al guardar los datos. Por favor, inténtalo de nuevo.');
     }
-
   };
 
   return (
-    
-    <div className="justify-center h-[calc(100vh-4rem)] flex items-center">
-      
-      <form onSubmit={handleSubmit} className="bg-neutral-950 px-8 py-10 w-3/12">
-        
-        <h1 className="text-4xl font-bold mb-7 text-slate-300">Registrar</h1>
+    <div className="flex items-center justify-center min-h-screen">
+      <form onSubmit={handleSubmit} className="bg-neutral-950 p-8 rounded-lg shadow-lg w-full max-w-md mx-4">
+        <h1 className="text-4xl font-bold mb-7 text-slate-300 text-center">Registrar</h1>
 
         <label className="text-slate-300">Nombre:</label>
         <input
@@ -67,7 +60,7 @@ function Signup() {
           value={fullName}
           type="text"
           placeholder="Santiago"
-          className="border-4 border-b-black px-4 py-2 block mb-2 w-full"
+          className="border-4 border-b-black px-4 py-2 block mb-2 w-full rounded-md"
           name="fullName"
           id="fullName"
         />
@@ -78,44 +71,38 @@ function Signup() {
           value={email}
           type="email"
           placeholder="santi@gmail.com"
-          className="border-4 border-b-black px-4 py-2 block mb-2 w-full"
+          className="border-4 border-b-black px-4 py-2 block mb-2 w-full rounded-md"
           name="email"
           id="email"
         />
 
         <label className="text-slate-300">Contraseña:</label>
         <input
-          onChange={(e) => setPassword(e.target.value)} // Corregido aquí
+          onChange={(e) => setPassword(e.target.value)}
           value={password}
           type="password"
           placeholder="*****************"
-          className="border-4 border-b-black px-4 py-2 block mb-2 w-full"
+          className="border-4 border-b-black px-4 py-2 block mb-2 w-full rounded-md"
           name="password"
           id="password"
         />
-        {/* Conditionally render error message */}
+
         {error && (
-          <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+          <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 mb-4 rounded-md" role="alert">
             <p className="font-bold">¡Atención!</p>
             <p>{error}</p>
           </div>
         )}
-        <button type="sudmit" className="bg-blue-500 text-white px-4 py-2 block w-full mt-4">
+        <button type="submit" className="bg-blue-500 text-white px-4 py-2 block w-full mt-4 rounded-md">
           Registrar
         </button>
-        <br/>
-        <a href="/" className="bg-green-50 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-            
-           
-            Login
-
-          </a>
         <br />
-
+        <a href="/" className="block text-center bg-green-50 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Login
+        </a>
+        <br />
       </form>
-      
     </div>
-    
   );
 }
 
